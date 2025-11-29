7918d65ea2dd3424cb61f42ae674c81070c37be4
Ganesh Thampi <ganeshthampi077@gmail.com>
2025-11-28 23:48:28 -0800

    Update fibonacci.asm to print decimal instead of hex

--- programs/fibonacci.asm
+++ programs/fibonacci.asm
@@ -1,6 +1,6 @@
 ; Fibonacci sequence demo
 .const IO_CONSOLE_DATA 0xFF00
-.const HEX_ALPHA_OFFSET 55
+
 .const FIB_COUNT 12
 
         .org 0x0000
@@ -10,7 +10,7 @@ start:
         LDI r2, #FIB_COUNT
 loop:
         MOV r3, r0          ; value to print
-        CALL print_hex
+        CALL print_decimal
         LDI r4, #10
         STORE r4, [IO_CONSOLE_DATA]
 
@@ -24,37 +24,47 @@ loop:
         HALT
 
 ; Prints R3 in hexadecimal (0x0000 format) and preserves caller registers
-print_hex:
+; Prints R3 in decimal and preserves caller registers
+print_decimal:
+        PUSH r0
         PUSH r4
         PUSH r5
         PUSH r6
+
+        ; Handle 0 special case
+        CMP r3, #0
+        JNZ init_extract
         LDI r4, #'0'
         STORE r4, [IO_CONSOLE_DATA]
-        LDI r4, #'x'
-        STORE r4, [IO_CONSOLE_DATA]
-        LDI r6, #12         ; starting shift amount
-hex_loop:
-        MOV r5, r3
-        SHR r5, r6
-        AND r5, #0x000F
-        LDI r4, #10
-        CMP r5, r4
-        JC letter
-        ADDI r5, #'0'
-        JMP write_digit
-letter:
-        ADDI r5, #HEX_ALPHA_OFFSET
-write_digit:
-        STORE r5, [IO_CONSOLE_DATA]
-        CMP r6, #0
-        JZ done_hex
-        SUBI r6, #4
-        JMP hex_loop
+        JMP done_decimal
 
-done_hex:
-        LDI r4, #' '
-        STORE r4, [IO_CONSOLE_DATA]
+init_extract:
+        LDI r6, #0          ; Digit count
+        LDI r4, #10         ; Divisor
+
+extract_loop:
+        MOV r5, r3          ; r5 = dividend
+        DIV r3, r4          ; r3 = quotient
+        MOV r0, r3          ; r0 = quotient
+        MUL r0, r4          ; r0 = quotient * 10
+        SUB r5, r0          ; r5 = dividend - (quotient * 10) = remainder
+        PUSH r5             ; Save digit
+        ADDI r6, #1         ; Increment count
+        CMP r3, #0
+        JNZ extract_loop
+
+print_loop:
+        CMP r6, #0
+        JZ done_decimal
+        POP r5
+        ADDI r5, #'0'       ; Convert to ASCII
+        STORE r5, [IO_CONSOLE_DATA]
+        SUBI r6, #1
+        JMP print_loop
+
+done_decimal:
         POP r6
         POP r5
         POP r4
+        POP r0
         RET
